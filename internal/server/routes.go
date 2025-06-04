0a31d7f2a5615222340745e4e1402b4d7ed08d0a
Neville Mehta <ndesk1900@gmail.com>
2025-06-04 00:31:42 -0700

    Update routes.go (tests should fail)

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -33,8 +33,8 @@ func NewMux(handler *tasks.TaskHandler) http.Handler {
 	mux := http.NewServeMux()
 
 	// Handler for /task (LIST and CREATE)
-	// mux.HandleFunc("GET /task", handler.ListTask)
-	mux.HandleFunc("POST /task", handler.CreateTask)
+	mux.HandleFunc("GET /task", handler.ListTask)
+	// mux.HandleFunc("POST /task", handler.CreateTask)
 
 	// Handler for /task/ (UPDATE and DELETE specific tasks)
 	mux.HandleFunc("UPDATE /task", handler.UpdateTask)
